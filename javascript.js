53c2c6d4bbf8dbc116cce8fa195a5611852dd34d
vishalpansar0 <vishalpansar0@gmail.com>
2021-09-09 23:09:33 +0530

    in

--- javascript.js
+++ javascript.js
@@ -1,23 +1,178 @@
-const shareBtn = document.getElementById('shareBtn');
+// const shareBtn = document.getElementById('shareBtn');
+// const startBtn = document.getElementById('startBtn');
+// const stopBtn = document.getElementById('stopBtn');
+// const vElement = document.getElementById('videoElement');
+// const download = document.getElementById('download');
+
+
+// let stream;
+// let desktopstream;
+// let blobs;
+//   let blob;
+//   let rec;
+  
+
+
+
+//   async function startcapture(){
+//     try {
+//       desktopStream = await navigator.mediaDevices.getDisplayMedia({video:true,audio: false});
+//     } catch (error) {
+//       console.log("ERRROR" + error);
+//     }
+    
+
+//     const tracks = [
+//       ...desktopStream.getVideoTracks()  ];
+
+//     stream = new MediaStream(tracks);
+//     console.log('Stream', stream)
+//     vElement.srcObject = stream; 
+//     blobs = [];
+    
+//     rec = new MediaRecorder(vElement.srcObject,{mimeType: 'video/webm;codecs=vp8'});
+//     rec.ondataavailable = (e) => blobs.push(e.data);
+//     rec.onstop = async () => {
+
+      
+      
+//       //blobs.push(MediaRecorder.requestData());
+//       blob = new Blob(blobs, {type: 'video/webm'});
+//       let url = window.URL.createObjectURL(blob);
+//       download.href = url;
+//       download.download = 'test.webm';
+//       download.style.display = 'block';
+//       download.click();
+//     };
+//   }
+//   function startrecorder(){
+//     rec.start();
+//   }
+
+//  function stopcapture(){
+//    rec.stop();
+
+
+   
+//    stream.getTracks().forEach(s=>s.stop())
+//     videoElement.srcObject = null
+//     stream = null;
+//  }
+
+//  shareBtn.addEventListener('click', () => {
+//   startcapture()
+// })
+
+// stopBtn.addEventListener('click', () => {
+//   startrecorder()
+// })
+
+// stopBtn.addEventListener('click', () => {
+//   stopcapture()
+// })
+
+
+const warningEl = document.getElementById('warning');
+const videoElement = document.getElementById('videoElement');
+const captureBtn = document.getElementById('captureBtn');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
-const vElement = document.getElementById('videoElement');
+const download = document.getElementById('download');
+const audioToggle = document.getElementById('audioToggle');
+const micAudioToggle = document.getElementById('micAudioToggle');
 
+// if('getDisplayMedia' in navigator.mediaDevices) warningEl.style.display = 'none';
 
+let blobs;
+let blob;
+let rec;
 let stream;
-let desktopstream;
+let voiceStream;
+let desktopStream;
 
-shareBtn.addEventListener('click', () => {
-    startcapture()
-  })
-
-  async function startcapture(){
-    desktopStream = await navigator.mediaDevices.getDisplayMedia({ video:true});
-
-    const tracks = [
-      ...desktopStream.getVideoTracks()   ];
-
-    stream = new MediaStream(tracks);
-    console.log('Stream', stream)
-    vElement.srcObject = stream;
+const mergeAudioStreams = (desktopStream, voiceStream) => {
+  const context = new AudioContext();
+  const destination = context.createMediaStreamDestination();
+  let hasDesktop = false;
+  let hasVoice = false;
+  if (desktopStream && desktopStream.getAudioTracks().length > 0) {
+    // If you don't want to share Audio from the desktop it should still work with just the voice.
+    const source1 = context.createMediaStreamSource(desktopStream);
+    const desktopGain = context.createGain();
+    desktopGain.gain.value = 0.7;
+    source1.connect(desktopGain).connect(destination);
+    hasDesktop = true;
   }
+  
+  if (voiceStream && voiceStream.getAudioTracks().length > 0) {
+    const source2 = context.createMediaStreamSource(voiceStream);
+    const voiceGain = context.createGain();
+    voiceGain.gain.value = 0.7;
+    source2.connect(voiceGain).connect(destination);
+    hasVoice = true;
+  }
+    
+  return (hasDesktop || hasVoice) ? destination.stream.getAudioTracks() : [];
+};
+
+captureBtn.onclick = async () => {
+  download.style.display = 'none';
+  const audio = audioToggle.checked || false;
+  const mic = micAudioToggle.checked || false;
+  
+  desktopStream = await navigator.mediaDevices.getDisplayMedia({ video:true, audio: audio });
+  
+  if (mic === true) {
+    voiceStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: mic });
+  }
+
+  const tracks = [
+    ...desktopStream.getVideoTracks(), 
+    ...mergeAudioStreams(desktopStream, voiceStream)
+  ];
+  
+  console.log('Tracks to add to stream', tracks);
+  stream = new MediaStream(tracks);
+  console.log('Stream', stream)
+  videoElement.srcObject = stream;
+  videoElement.muted = true;
+    
+  blobs = [];
+
+  rec = new MediaRecorder(stream, {mimeType: 'video/webm; codecs=vp8,opus'});
+  rec.ondataavailable = (e) => blobs.push(e.data);
+  rec.onstop = async () => {
+    
+    //blobs.push(MediaRecorder.requestData());
+    blob = new Blob(blobs, {type: 'video/webm'});
+    let url = window.URL.createObjectURL(blob);
+    download.href = url;
+    download.download = 'test.webm';
+    download.style.display = 'block';
+    download.click();
+  };
+  startBtn.disabled = false;
+  captureBtn.disabled = true;
+  audioToggle.disabled = true;
+  micAudioToggle.disabled = true;
+};
+
+startBtn.onclick = () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  rec.start();
+};
+
+stopBtn.onclick = () => {
+  captureBtn.disabled = false;
+  audioToggle.disabled = false;
+  micAudioToggle.disabled = false;
+  startBtn.disabled = true;
+  stopBtn.disabled = true;
+  
+  rec.stop();
+  
+  stream.getTracks().forEach(s=>s.stop())
+  videoElement.srcObject = null
+  stream = null;
+};
