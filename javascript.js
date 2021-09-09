8e6931c691f93f9c958e7a9700ab51fc42131e07
vishalpansar0 <vishalpansar0@gmail.com>
2021-09-09 19:08:17 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,9 +2,14 @@ const shareBtn = document.getElementById('shareBtn');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 
+
+let stream;
 let desktopstream;
 
 shareBtn.addEventListener('click', () => {
     desktopstream = navigator.mediaDevices.getDisplayMedia({video: true});
-        
+    const track = desktopstream.getVideoTracks()[0];
+
+    stream = new MediaStream(track);
+    console.log('Stream', stream);
   })
